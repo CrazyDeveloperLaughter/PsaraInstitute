a3ac471a7a8f4df52366e96dc8516b0f8305f564
SeanYamaguchi <shion.yamaguchi.shion@gmail.com>
2015-06-28 06:05:06 +0900

    chenge how to email

--- config/environment.rb
+++ config/environment.rb
@@ -7,3 +7,17 @@ Encoding.default_external = Encoding::UTF_8
 Encoding.default_internal = Encoding::UTF_8
 
 Rails.application.initialize!
+
+ActionMailer::perform_deliveries = true
+ActionMailer::Base.raise_delivery_errors = true
+ActionMailer::Base.default_url_options = { :host => 'psara-institute.com' }
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.smtp_settings = {
+  :address => 'smtp.sendgrid.net',
+  :port => '587',
+  :authentication => "plain",
+  :user_name => 'app34862435@heroku.com',
+  :password => 'kbrhc1jk',
+  :domain => 'heroku.com',
+  :enable_starttls_auto => true
+}
