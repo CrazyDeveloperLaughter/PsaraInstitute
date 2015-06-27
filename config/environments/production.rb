ceca80d5a76b213bcf1e718bc3bf9c26063d9a7d
SeanYamaguchi <shion.yamaguchi.shion@gmail.com>
2015-06-28 07:55:43 +0900

    chenge how to email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,7 +57,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
   :address => 'smtp.sendgrid.net',
   :port => 587,
-  :authentication => "plain",
+  :authentication => "login",
   :user_name => 'ENV[SENDGRID_USERNAME]',
   :password => 'ENV[SENDGRID_PASSWORD]',
   :domain => 'muumuu-domain.com',
