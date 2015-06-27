40164132849ffacb8c2aa8341e5d62aec4147c29
SeanYamaguchi <shion.yamaguchi.shion@gmail.com>
2015-06-28 06:18:44 +0900

    chenge how to email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,8 +55,8 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { :host => 'psara-institute.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-  :address => 'smtp.gmail.com',
-  :port => '587',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
   :authentication => "plain",
   :user_name => 'app34862435@heroku.com',
   :password => 'kbrhc1jk',
