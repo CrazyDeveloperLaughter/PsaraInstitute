dc11589dfb18c812281dc332689762319122a264
SeanYamaguchi <shion.yamaguchi.shion@gmail.com>
2015-06-28 07:27:07 +0900

    chenge how to email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,7 @@ Rails.application.configure do
   :authentication => "plain",
   :user_name => 'ENV[SENDGRID_USERNAME]',
   :password => 'ENV[SENDGRID_PASSWORD]',
-  :domain => 'psara-institute.com',
+  :domain => 'muumuu-domain.com',
   :enable_starttls_auto => true
   }
 end
